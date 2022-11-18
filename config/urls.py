fe45754225c201332481cfcb1360bc198d627db6
Jochen Klar <mail@jochenklar.de>
2022-11-18 15:03:19 +0100

    Move urls to django_datacite with proper namespacing

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include
 from django.views.generic.base import RedirectView
-
-from django_datacite.views import resource, resource_json, resource_xml
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', include('django_datacite.urls', namespace='datacite')),
     path('', RedirectView.as_view(pattern_name='admin:index')),
-    re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+).xml$', resource_xml, name='resource_xml'),
-    re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+).json$', resource_json, name='resource_json'),
-    re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+)', resource, name='resource')
 ]
